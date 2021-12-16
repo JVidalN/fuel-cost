72c0015e4f0e092250731db60a7865b16c9cfe2a
JVidalN <j.vidalnunes@gmail.com>
2021-12-15 23:39:43 -0300

    Included simple calculate

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 calculateCost;
 function calculateCost(form) {
+  form.kmmonth.value = 4 * form.kmday.value * form.dayrun.value;
+  form.kmyear.value = form.kmmonth.value * 12;
+  form.costmonth.value =
+    (form.kmday.value / form.kml.value) * form.fuelcost.value * form.dayrun.value * 4;
+  form.costyear.value = form.costmonth.value * 12;
+
   return false;
 }
