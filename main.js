49133db3c7250a61e078ea7638ded39f28cf4c8e
JVidalN <j.vidalnunes@gmail.com>
2021-12-18 23:29:48 -0300

    Changed form inline. Included format decimal currency and more keywords.

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 calculateCost;
 function calculateCost(form) {
-  form.kmmonth.value = 4 * form.kmday.value * form.dayrun.value;
-  form.kmyear.value = form.kmmonth.value * 12;
-  form.costmonth.value =
-    (form.kmday.value / form.kml.value) * form.fuelcost.value * form.dayrun.value * 4;
-  form.costyear.value = form.costmonth.value * 12;
+    validForm(form);
+    form.kmmonth.value = 4 * form.kmday.value * form.dayrun.value;
+    form.kmyear.value = form.kmmonth.value * 12;
+    form.costmonth.value = (
+        (form.kmday.value / form.kml.value) *
+        form.fuelcost.value *
+        form.dayrun.value *
+        4
+    ).toFixed(2);
+    form.costyear.value = (form.costmonth.value * 12).toFixed(2);
 
-  return false;
+    return false;
+}
+
+function validForm(form) {
+    if (
+        form.kmday.value === '' ||
+        form.dayrun.value === '' ||
+        form.fuelcost.value === '' ||
+        form.kml.value === ''
+    ) {
+        alert('Parametros invalidos');
+
+        return false;
+    }
 }
